6d7f08bd009fbae6e64dbd1b67e0d1f684f547d6
tom1103 <tom1103@users.noreply.github.com>
2015-03-21 16:25:50 +0100

    Add lenght + defaut value

--- view.py
+++ view.py
@@ -5,18 +5,24 @@ class Interface(Frame):
     
     def __init__(self, fen):
         Frame.__init__(self, fen, width=800, height=530)
-        self.pack(fill=BOTH)
+        self.pack()
         
-        self.lenght = Spinbox(fen, from_=0, to=100)
-        self.lenght.pack()        
+        self.label_lenght = Label(self, text="Password lenght :")
+        self.label_lenght.pack(side=TOP)
+        self.lenght = Spinbox(self, from_=1, to=100, width=5)
+        self.lenght.delete(0, END)
+        self.lenght.insert(0, 25)
+        self.lenght.pack(side=TOP)
+        
         self.label_resultat = Label(self, text="Password :")
-        self.label_resultat.pack()
+        self.label_resultat.pack(side=TOP)
         self.message_resultat = Entry(self, width=30, font=("Verdana", 12))
-        self.message_resultat.pack()
+        self.message_resultat.pack(side=TOP)
 
         
         self.ok_btn = Button(fen, text="Generate", command=self.generate)
-        self.ok_btn.pack()
+        self.ok_btn.pack(side=TOP)
+
 
     def generate(self,event=None):
         lenght = int(self.lenght.get())
