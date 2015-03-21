c3479ab5149d10f8c7a33979a5a58c0f7437cc24
tom1103 <tom1103@users.noreply.github.com>
2015-03-21 17:17:12 +0100

    Add copy button Add focus on password when copy button click

--- view.py
+++ view.py
@@ -23,12 +23,25 @@ class Interface(Frame):
         self.ok_btn = Button(fen, text="Generate", command=self.generate)
         self.ok_btn.pack(side=TOP)
 
-
+        self.copy_btn = Button(fen, text="Copy", command=self.copy)
+        self.copy_btn.pack(side=RIGHT)
+        
     def generate(self,event=None):
         lenght = int(self.lenght.get())
         self.message_resultat.delete(0, END)
         self.message_resultat.insert(0, gen(lenght))
-         
+        
+    def copy(self):
+        # effacer l'éventuel contenu précédent du clipboard
+        self.clipboard_clear()
+        try:
+            # saisir la sélection s'il y en a une (sinon -> except)
+            t = self.message_resultat.get()
+            self.message_resultat.selection_range(0, END)
+            # et envoyer le texte sélectionné dans le clipboard
+            self.clipboard_append(t)
+        except:
+            pass
 
 app = Interface(Tk())
 app.mainloop()
