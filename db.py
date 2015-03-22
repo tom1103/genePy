67ac8ffbe357ce19a9afff69813ac5010028db4f
tom1103 <tom1103@users.noreply.github.com>
2015-03-22 21:33:06 +0100

    Initial change

--- db.py
+++ db.py
@@ -3,6 +3,7 @@ import sqlite3
 db_name = "./db/pass.sq3"
 db_path = "./db/"
 
+
 conn = sqlite3.connect("pass.sq3")
 cur = conn.cursor()
 
@@ -10,18 +11,23 @@ cur = conn.cursor()
 # Création de la table
 
 def createtable():
-    cur.execute("CREATE TABLE pass (id TEXT, login TEXT, pass TEXT)")
+    cur.execute("CREATE TABLE pass (id INTEGER, login TEXT, pass TEXT)")
 
 def testdata():
     i = 0
-    while i<10:
-        cur.execute("INSERT INTO pass(id, login, pass) VALUES(1, test, test2)")
-        i += i
+    while i<100:
+        cur.execute("INSERT INTO pass(id,login,pass) VALUES(:i,'test3','test332')", {"i":i})
+        i = i+1
     conn.commit()
 
+
 def readdata():
-    cur.execute()
+    cur.execute("SELECT * FROM 'pass'")
+    print(cur.fetchall())
 
 # createtable()
 testdata()
+readdata()
 
+cur.close()
+conn.close()
