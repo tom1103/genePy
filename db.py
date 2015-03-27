20f8b59b8b6be9f88f338dcce645a4ca4781b114
Toma <tom1103@users.noreply.github.com>
2015-03-27 15:25:46 +0100

    initial commit for db

--- db.py
+++ db.py
@@ -4,30 +4,33 @@ db_name = "./db/pass.sq3"
 db_path = "./db/"
 
 
-conn = sqlite3.connect("pass.sq3")
-cur = conn.cursor()
+
+class Db():
+    """Classe Db pour interraction avec BDD"""
+    def __init__(self):
+        self.conn = sqlite3.connect("pass.sq3")
+        self.cur = self.conn.cursor()
+        self._createTable()
+
+    def _createTable(self):
+        """Creation de la table si non existante"""
+        self.cur.execute("CREATE TABLE IF NOT EXISTS password(login TEXT, passw TEXT, note TEXT)")
 
 
-# Création de la table
+    def dataInsert(self, login, passw, note):
+        """Insertion data : login, passw, note"""
+            i = 0
+            while i<1000000:
+                self.cur.execute("INSERT INTO password(login, passw, note) VALUES(:login, :passw, :note)", {"login":login, "passw":passw, "note":note})
+                i = i+1
+            self.conn.commit()
 
-def createtable():
-    cur.execute("CREATE TABLE pass (id INTEGER, login TEXT, pass TEXT)")
+    def readdata(self):
+        """Lecture de la table"""
+        self.cur.execute("SELECT * FROM 'password'")
+        print(self.cur.fetchall())
 
-def testdata():
-    i = 0
-    while i<100:
-        cur.execute("INSERT INTO pass(id,login,pass) VALUES(:i,'test3','test332')", {"i":i})
-        i = i+1
-    conn.commit()
-
-
-def readdata():
-    cur.execute("SELECT * FROM 'pass'")
-    print(cur.fetchall())
-
-# createtable()
-testdata()
-readdata()
-
-cur.close()
-conn.close()
+    def close(self):
+        """Fermeture du curseur et de la connection"""
+        self.cur.close()
+        self.conn.close()
